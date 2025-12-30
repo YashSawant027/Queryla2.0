f5343322322ecdc2395888acd5043fc194687b62
YashSawant027 <yashsawan07@gmail.com>
2025-12-30 16:44:24 +0530

    deploymenttttttttt

--- front/src/Components/Register.jsx
+++ front/src/Components/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
 
         try{
             setloading(true)
-            const response = await axios.post('http://127.0.0.1:8000/api/v1/RegisterPage', userdata)
+            const response = await axios.post('http://0.0.0.0:8000/api/v1/RegisterPage', userdata)
             console.log("Registration successfull")
             seterror({})
             navigate('/login')
